3455dd2d552d4f0a05afaab4142f9be106510892
dohaoutahar <outahar.doha@etu.uae.ac.ma>
2026-03-27 18:27:29 +0100

    feat: ajout de Department et Teacher dans l'interface d'administration

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Department, Teacher
 
-# Register your models here.
+# Enregistrement du modèle Department
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('department_id', 'department_name', 'head_of_department', 'no_of_students')
+    search_fields = ('department_id', 'department_name')
+
+# Enregistrement du modèle Teacher
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'teacher_id', 'department', 'mobile_number')
+    search_fields = ('first_name', 'last_name', 'teacher_id')
+    list_filter = ('gender', 'department')
